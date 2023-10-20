5831e8c3e150217ca7fffaa921b9d9b5d1047a52
Zuhra Husanova <148433060+ZuhraHusanova@users.noreply.github.com>
2023-10-20 19:39:58 +0500

    Add files via upload

---
+++ YOUTUBE/YOUTUBE/main.js
@@ -0,0 +1,162 @@
+let box =
+[
+    {
+        "id":1,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"15K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":2,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"Alan Cooper",
+        "wiews":"18K",
+        "when":"1 week ago",
+        "user":"https://www.youtube.com/watch?v=aTHBJwVgu3I",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":3,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"Marcus Levin",
+        "wiews":"15K",
+        "when":"Alexis Sears",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":4,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"20K",
+        "when":"1 week ago",
+        "user":"https://www.youtube.com/watch?v=SjDFJCCB6bs&t=827s",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":5,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"Jesica Lambert",
+        "wiews":"11K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":6,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"Anna White",
+        "wiews":"9K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":7,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"Skylar Dias",
+        "wiews":"1K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":8,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"3K",
+        "when":"1 week ago",
+        "user":"https://www.youtube.com/watch?v=M5QY2_8704o&t=4725s&pp=ygUMY29kaW5nIG11c2lj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":9,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"15K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":10,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"2K",
+        "when":"1 week ago",
+        "user":"https://www.youtube.com/watch?v=1LH5JcJtV94&t=1702s&pp=ygUMY29kaW5nIG11c2lj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":11,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"15K",
+        "when":"1 week ago",
+        "user":"https://www.youtube.com/watch?v=W0VpO8ZaqNs&t=2317s&pp=ygUMY29kaW5nIG11c2lj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+    },
+    {
+        "id":12,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"8K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":13,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"6K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":14,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"17K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":14,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"23K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    },
+    {
+        "id":15,
+        "title":"Lorem ipsum dolor sit amet, consecte adipiscing elit.",
+        "name":"James Gouse",
+        "wiews":"19K",
+        "when":"1 week ago",
+        "user":"https://yt3.ggpht.com/94e4wzm6r8_N_eUurrqw5jlHrzRX5K1l9zvgoF49GepiX_8Ut-IbQDNs-3w8gL3K-KKuSE8d-Q=s68-c-k-c0x00ffffff-no-rj",
+        "url":"https://www.youtube.com/watch?v=qIqAFibHQXg"
+
+    }
+]
